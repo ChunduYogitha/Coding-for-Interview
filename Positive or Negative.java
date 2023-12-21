8da7ade70110581eedc8201e7c8984fc9cce8be6
ChunduYogitha <133772386+ChunduYogitha@users.noreply.github.com>
2023-12-21 14:47:58 +0530

    Positive or Negative.java

---
+++ Positive or Negative.java
@@ -0,0 +1,23 @@
+Check the number is positive or negative 
+
+  
+import java.io.*;
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if(a>=0){
+            System.out.println("Positive");
+        }
+        else{
+            System.out.println("Negative");
+        }
+    }
+}
+
+INPUT : 4
+OUTPUT : Positive
+
+  -----------------------------------  -----------------------------------  -----------------------------------  -----------------------------------  -----------------------------------
+        
