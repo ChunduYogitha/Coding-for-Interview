cb57c4351e082025d4f639943da4369a316e2b4a
ChunduYogitha <133772386+ChunduYogitha@users.noreply.github.com>
2023-12-21 15:34:13 +0530

    Positive or Negative.java

--- Positive or Negative.java
+++ Positive or Negative.java
@@ -43,3 +43,70 @@ public class Main{
         System.out.println("For loop is ended");
     }
 }
+INPUT :Enter n = 3
+OUTPUT :2
+        Positive
+        -5
+        Negative
+        8
+        Positive
+
+
+-----------------------------------  -----------------------------------  -----------------------------------  -----------------------------------  -----------------------------------
+QUE : Using call method or functions for only one time
+  
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        PosOrNeg(a);
+    
+    }
+
+    static void PosOrNeg(int a) {
+        
+        if (a >= 0) {
+            System.out.println("Positive");
+        } else {
+            System.out.println("Negative");
+        }
+    }
+}
+INPUT : 5
+OUTPUT: Positive
+        
+  
+  
+  -----------------------------------  -----------------------------------  -----------------------------------  -----------------------------------  -----------------------------------
+
+  QUE : Using call method or functions for mutiple inputs 
+
+  
+  import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=0;i<n;i++){
+        int a = sc.nextInt();
+        PosOrNeg(a);
+    }
+    }
+
+    static void PosOrNeg(int a) {
+        
+        if (a >= 0) {
+            System.out.println("Positive");
+        } else {
+            System.out.println("Negative");
+        }
+    }
+}
+INPUT : 2
+OUTPUT : 9
+        Positive
+          -2
+        Negative
