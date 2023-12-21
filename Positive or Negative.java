d267c4c309d72c69fd649cbd9986944878322114
ChunduYogitha <133772386+ChunduYogitha@users.noreply.github.com>
2023-12-21 15:07:44 +0530

    Positive or Negative.java

--- Positive or Negative.java
+++ Positive or Negative.java
@@ -20,4 +20,26 @@ INPUT : 4
 OUTPUT : Positive
 
   -----------------------------------  -----------------------------------  -----------------------------------  -----------------------------------  -----------------------------------
-        
+
+Using for loop
+  
+import java.io.*;
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n = ");
+        int n = sc.nextInt();
+        for(int i=0;i<n;i++){
+            System.out.println()
+        int a = sc.nextInt();
+        if(a>=0){
+            System.out.println("Positive");
+        }
+        else{
+            System.out.println("Negative");
+        }
+        }
+        System.out.println("For loop is ended");
+    }
+}
